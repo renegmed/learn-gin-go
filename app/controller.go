d2075039f36ed003c5204343365b9cc72cc5eab5
rene <renegmed@yahoo.com>
2019-02-07 00:23:25 -0500

    master: basic authentication

--- app/controller.go
+++ app/controller.go
@@ -10,6 +10,7 @@ func RegisterRoutes() *gin.Engine {
 
 	r := gin.Default()
 	r.LoadHTMLGlob("templates/**/*.html")
+
 	r.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.html", nil)
 	})
@@ -34,8 +35,12 @@ func RegisterRoutes() *gin.Engine {
 				"TimesOff": timesOff,
 			})
 	})
+
 	// http://localhost:3000/admin
-	admin := r.Group("/admin")
+	admin := r.Group("/admin", gin.BasicAuth(gin.Accounts{
+		"admin": "admin",
+	}))
+
 	admin.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "admin-overview.html", nil)
 	})
