6040008d78a8e6b07ef8fa937b360d0f23fa1502
rene <renegmed@yahoo.com>
2019-02-07 00:16:03 -0500

    master: parameterized routes

--- app/controller.go
+++ app/controller.go
@@ -19,13 +19,28 @@ func RegisterRoutes() *gin.Engine {
 		c.HTML(http.StatusOK, "login.html", nil)
 	})
 
+	// e.g. localhost:3000/employees/962134/vacation
+	r.GET("/employees/:id/vacation", func(c *gin.Context) {
+		id := c.Param("id")
+		timesOff, ok := TimesOff[id]
+
+		if !ok {
+			c.String(http.StatusNotFound, "404 - Page Not Found")
+			return
+		}
+
+		c.HTML(http.StatusOK, "vacation-overview.html",
+			map[string]interface{}{
+				"TimesOff": timesOff,
+			})
+	})
 	// http://localhost:3000/admin
 	admin := r.Group("/admin")
 	admin.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "admin-overview.html", nil)
 	})
 
-	r.Static("/public", "./public")
+	r.Static("/public", "../public")
 	//   or
 	// r.StaticFS("/public", http.Dir("./public"))
 
